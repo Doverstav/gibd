6ef6a794e1d3207d6bd290646a18d63c85c9e378
Pontus Doverstav <doverstav@gmail.com>
2022-08-07 17:13:22 +0200

    Fix some remote stuff

--- scripts/setupTestBranches.sh
+++ scripts/setupTestBranches.sh
@@ -3,25 +3,32 @@ set -e
 
 echo "Setting up test branches"
 
+# Ensure we start from master
+echo "Checking out master"
+git checkout master &> /dev/null
+
 # Setup branch that is only local
 echo "Creating local only branch"
 git branch localOnly &> /dev/null
 
 # Setup branch with remote existing
 echo "Creating branch with remote existing"
-git branch remoteExisting &> /dev/null
+git checkout -b remoteExisting &> /dev/null
 git push -u origin remoteExisting &> /dev/null
+git checkout master &> /dev/null
 
 # Setup branch that has remote gone
 echo "Creating branch with remote gone"
-git branch remoteDeleted &> /dev/null
+git checkout -b remoteDeleted &> /dev/null
 git push -u origin remoteDeleted &> /dev/null
 git push -d origin remoteDeleted &> /dev/null
+git checkout master &> /dev/null
 
 # Setup branch that gives merged warning
 echo "Creating branch with merge warning"
 git checkout -b mergeWarning &> /dev/null
 git commit -m "Test" --allow-empty &> /dev/null
+git checkout master &> /dev/null
 
 # Return to master
 echo "Returning to master"
