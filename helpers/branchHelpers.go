d6b7ff8750322dfcf9600afc82c26c81796afd9c
Pontus Doverstav <doverstav@gmail.com>
2022-08-13 17:35:31 +0200

    Add helper for getting branches with remote status [gone]

--- helpers/branchHelpers.go
+++ helpers/branchHelpers.go
@@ -1,7 +1,6 @@
 package helpers
 
 import (
-	"fmt"
 	"strings"
 )
 
@@ -9,12 +8,33 @@ func GetBranchNames(branches []string) []string {
 	branchNames := []string{}
 
 	for _, branchString := range branches {
-		splitString := strings.Split(branchString, " ")
-		fmt.Println(splitString)
+		splitString := strings.Split(strings.TrimSpace(branchString), " ")
+
 		branchRef := splitString[0]
 
 		branchName := strings.TrimPrefix(strings.TrimSpace(branchRef), "refs/heads/")
 		branchNames = append(branchNames, branchName)
+	}
+
+	return branchNames
+}
+
+func GetBranchNamesWithRemoteGone(branches []string) []string {
+	const DESIRED_REMOTE_STATUS = "[gone]"
+	branchNames := []string{}
+
+	for _, branchString := range branches {
+		splitString := strings.Split(strings.TrimSpace(branchString), " ")
+		if len(splitString) == 2 {
+
+			branchRef := splitString[0]
+			remoteStatus := splitString[1]
+
+			if remoteStatus == DESIRED_REMOTE_STATUS {
+				branchName := strings.TrimPrefix(strings.TrimSpace(branchRef), "refs/heads/")
+				branchNames = append(branchNames, branchName)
+			}
+		}
 	}
 
 	return branchNames
